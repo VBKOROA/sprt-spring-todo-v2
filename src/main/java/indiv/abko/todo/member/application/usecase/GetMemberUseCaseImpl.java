fb3cb34a6367a8152f3e967158fc6c01d3f464c1
abko <abko121115@gmail.com>
2025-08-05 20:43:57 +0900

    feat: GetMemberUseCaseImpl.get 구현

---
+++ src/main/java/indiv/abko/todo/member/application/usecase/GetMemberUseCaseImpl.java
@@ -0,0 +1,21 @@
+package indiv.abko.todo.member.application.usecase;
+
+import indiv.abko.todo.global.exception.BusinessException;
+import indiv.abko.todo.member.application.port.in.GetMemberUseCase;
+import indiv.abko.todo.member.domain.Member;
+import indiv.abko.todo.member.domain.MemberExceptionEnum;
+import indiv.abko.todo.member.domain.port.out.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class GetMemberUseCaseImpl implements GetMemberUseCase {
+    private final MemberRepository memberRepo;
+
+    @Override
+    public Member get(long id) {
+        return memberRepo.findById(id)
+                .orElseThrow(() -> new BusinessException(MemberExceptionEnum.MEMBER_NOT_FOUND));
+    }
+}
