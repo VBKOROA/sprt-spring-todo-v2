781f40c84d9cb175b7671074c6ae83ff08fb5f2d
abko <abko121115@gmail.com>
2025-08-05 20:37:18 +0900

    feat: MemberExceptionEnum.MEMBER_NOT_FOUND 구현

---
+++ src/main/java/indiv/abko/todo/member/domain/MemberExceptionEnum.java
@@ -0,0 +1,15 @@
+package indiv.abko.todo.member.domain;
+
+import indiv.abko.todo.global.exception.BusinessExceptionEnum;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum MemberExceptionEnum implements BusinessExceptionEnum {
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 ID의 Member가 없습니다.");
+
+    private final HttpStatus status;
+    private final String message;
+}
