8c9a888d9ba106ef1ae3f02a9eab0b0a0c95a9f8
abko <abko121115@gmail.com>
2025-08-05 20:28:56 +0900

    feat: Member 도메인 초기 구현

---
+++ src/main/java/indiv/abko/todo/member/domain/Member.java
@@ -0,0 +1,18 @@
+package indiv.abko.todo.member.domain;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Member {
+    private String name; // 이름
+    private String email; // 이메일 (로그인용 ID)
+    private LocalDateTime modifiedAt; // 수정 시간
+    private LocalDateTime createdAt; // 생성 시간
+}
