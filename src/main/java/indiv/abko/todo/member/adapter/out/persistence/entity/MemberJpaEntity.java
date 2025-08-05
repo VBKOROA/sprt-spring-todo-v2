ce2c53ff3657bc43a3682b75daa3f29ae838e25e
abko <abko121115@gmail.com>
2025-08-05 20:08:18 +0900

    feat: MemberJpaEntity 초기 구현

---
+++ src/main/java/indiv/abko/todo/member/adapter/out/persistence/entity/MemberJpaEntity.java
@@ -0,0 +1,25 @@
+package indiv.abko.todo.member.adapter.out.persistence.entity;
+
+import indiv.abko.todo.global.entity.BaseTimeJpaEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Table(name = "member")
+@Getter
+@NoArgsConstructor
+@SuperBuilder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class MemberJpaEntity extends BaseTimeJpaEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+}
