dfbae17dd89be9640d7b1df5ea511b9f8a51bff2
abko <abko121115@gmail.com>
2025-08-06 20:09:52 +0900

    fix: remove unnecessary validation annotations from TodoCreateReq

--- src/main/java/indiv/abko/todo/todo/adapter/in/rest/dto/todo/TodoCreateReq.java
+++ src/main/java/indiv/abko/todo/todo/adapter/in/rest/dto/todo/TodoCreateReq.java
@@ -1,7 +1,5 @@
 package indiv.abko.todo.todo.adapter.in.rest.dto.todo;
 
-import indiv.abko.todo.todo.adapter.in.rest.validation.ValidTodoContent;
-import indiv.abko.todo.todo.adapter.in.rest.validation.ValidTodoTitle;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
@@ -9,12 +7,10 @@ import lombok.Builder;
 @Schema(description = "새 Todo 생성 요청")
 @Builder
 public record TodoCreateReq(
-    @ValidTodoTitle
     @NotBlank
     @Schema(description = "Todo 제목 / 1~30자 이내", example = "테스트", requiredMode = Schema.RequiredMode.REQUIRED)
     String title,
 
-    @ValidTodoContent
     @NotBlank
     @Schema(description = "Todo 내용 / 1~200자 이내", example = "테스트", requiredMode = Schema.RequiredMode.REQUIRED)
     String content
