aadad105f73ddc7df98943fd399a201d22d79c66
abko <abko121115@gmail.com>
2025-08-06 09:02:18 +0900

    refactor: 이름 변경

--- src/main/java/indiv/abko/todo/member/application/usecase/DefaultGetMemberUseCase.java
+++ src/main/java/indiv/abko/todo/member/application/usecase/DefaultGetMemberUseCase.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class GetMemberUseCaseImpl implements GetMemberUseCase {
+public class DefaultGetMemberUseCase implements GetMemberUseCase {
     private final MemberRepository memberRepo;
 
     @Override
