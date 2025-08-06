84216b1fc6875543e388849543a25f9999a95523
abko <abko121115@gmail.com>
2025-08-06 20:09:46 +0900

    fix: remove unnecessary validation annotation from CommentWriteReq

--- src/main/java/indiv/abko/todo/todo/adapter/in/rest/dto/comment/CommentWriteReq.java
+++ src/main/java/indiv/abko/todo/todo/adapter/in/rest/dto/comment/CommentWriteReq.java
@@ -1,12 +1,10 @@
 package indiv.abko.todo.todo.adapter.in.rest.dto.comment;
 
-import indiv.abko.todo.todo.adapter.in.rest.validation.ValidCommentContent;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
 @Schema(description = "댓글 작성 DTO")
 public record CommentWriteReq(
-    @ValidCommentContent
     @NotBlank
     @Schema(description = "댓글 내용 / 1~200자 이내", example = "테스트 내용", requiredMode = Schema.RequiredMode.REQUIRED)
     String content
