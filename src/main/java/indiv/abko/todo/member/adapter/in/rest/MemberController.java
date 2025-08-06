5398c8b284286c14131708c3f4c1837e2cf2045b
abko <abko121115@gmail.com>
2025-08-06 12:50:10 +0900

    fix: 오타 수정

--- src/main/java/indiv/abko/todo/member/adapter/in/rest/MemberController.java
+++ src/main/java/indiv/abko/todo/member/adapter/in/rest/MemberController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/member")
+@RequestMapping("/api/v1/members")
 public class MemberController {
     private final GetMemberUseCase getMemberUseCase;
     private final SignUpUseCase signUpUseCase;
