4b51f48e2b0a0c3ab0a086c4995dfa10aa62f28b
abko <abko121115@gmail.com>
2025-08-06 09:11:19 +0900

    feat: MemberJpaRepository.existsByEmail 이메일(로그인ID) 중복 체크

--- src/main/java/indiv/abko/todo/member/adapter/out/persistence/MemberJpaRepository.java
+++ src/main/java/indiv/abko/todo/member/adapter/out/persistence/MemberJpaRepository.java
@@ -4,4 +4,5 @@ import indiv.abko.todo.member.adapter.out.persistence.entity.MemberJpaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberJpaRepository extends JpaRepository<MemberJpaEntity, Long> {
+    boolean existsByEmail(String email);
 }
