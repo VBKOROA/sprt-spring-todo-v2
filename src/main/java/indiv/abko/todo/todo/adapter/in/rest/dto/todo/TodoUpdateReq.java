6c301c67afc34c894d5525214bcac9696a77b17d
abko <abko121115@gmail.com>
2025-08-06 20:09:58 +0900

    fix: update TodoUpdateReq schema description and required mode

--- src/main/java/indiv/abko/todo/todo/adapter/in/rest/dto/todo/TodoUpdateReq.java
+++ src/main/java/indiv/abko/todo/todo/adapter/in/rest/dto/todo/TodoUpdateReq.java
@@ -1,14 +1,12 @@
 package indiv.abko.todo.todo.adapter.in.rest.dto.todo;
 
-import indiv.abko.todo.todo.adapter.in.rest.validation.OptionalNotBlank;
-import indiv.abko.todo.todo.adapter.in.rest.validation.ValidTodoTitle;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
 @Schema(description = "Todo 업데이트 요청 DTO")
 public record TodoUpdateReq(
     @NotBlank
-    @Schema(description = "Todo 작성자", example = "수정할 작성자", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    @Schema(description = "Todo 내용", example = "할 일은 이것", requiredMode = Schema.RequiredMode.REQUIRED)
     String content
 ) {
 
