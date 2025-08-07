b8ef8fb389b52bb4b164e730346950e3d263ae0f
abko <abko121115@gmail.com>
2025-08-07 15:15:30 +0900

    fix: 정확한 http status를 사용하도록 수정

--- src/main/java/indiv/abko/todo/todo/domain/exception/TodoExceptionEnum.java
+++ src/main/java/indiv/abko/todo/todo/domain/exception/TodoExceptionEnum.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum TodoExceptionEnum implements BusinessExceptionEnum {
     TODO_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 ID의 일정이 없습니다."),
-    TODO_PERMISSION_DENIED(HttpStatus.UNAUTHORIZED, "해당 일정에 대한 권한이 없습니다."),
+    TODO_PERMISSION_DENIED(HttpStatus.FORBIDDEN, "해당 일정에 대한 권한이 없습니다."),
     COMMENT_LIMIT_EXCEEDED(HttpStatus.BAD_REQUEST, "댓글은 10개를 초과할 수 없습니다."),
     TODO_TITLE_LENGTH_NOT_VALID(HttpStatus.BAD_REQUEST, "제목의 길이는 1 ~ 30자 이내여야 합니다."),
     TODO_TITLE_REQUIRED(HttpStatus.BAD_REQUEST, "제목은 필수 입력 값입니다."),
